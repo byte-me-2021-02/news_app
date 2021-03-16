8556c235da92f73572ba2ea338aecba32201387b
brian <riceb53@gmail.com>
2021-03-16 19:21:42 -0400

    add request from controller

--- app/controllers/api/news_controller.rb
+++ app/controllers/api/news_controller.rb
@@ -1,2 +1,7 @@
 class Api::NewsController < ApplicationController
+  def index
+    # if i want data, go to the db
+    response = HTTP.get("http://newsapi.org/v2/everything?q=#{params[:search]}&from=2021-02-16&sortBy=publishedAt&apiKey=")
+    render json: response.parse
+  end
 end
