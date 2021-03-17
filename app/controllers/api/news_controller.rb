a522de2eae0ba9cf8f50359fe5a063849e7ba2e1
brian <riceb53@gmail.com>
2021-03-16 20:05:56 -0400

    hide api key

--- app/controllers/api/news_controller.rb
+++ app/controllers/api/news_controller.rb
@@ -1,7 +1,9 @@
 class Api::NewsController < ApplicationController
   def index
     # if i want data, go to the db
-    response = HTTP.get("http://newsapi.org/v2/everything?q=#{params[:search]}&from=2021-02-16&sortBy=publishedAt&apiKey=")
+    
+    
+    response = HTTP.get("http://newsapi.org/v2/everything?q=#{params[:search]}&from=2021-02-16&sortBy=publishedAt&apiKey=#{Rails.application.credentials.news_api[:api_key]}")
     render json: response.parse
   end
 end
